3536da34148b55b36817d78aec85c90850b36526
luis rios <lrioscoutino@gmail.com>
2026-02-16 20:38:17 -0600

    second commit

--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from users.models import Product
 
 # Create your views here.
 def get_user(request):
     return HttpResponse("Hello World")
 
 def inicio(request):
+    products = Product.objects.all()
     context = {
         "name":"Roberto Carlos",
         "email":"r@gmail.com",
         "age": 35,
-        "resumen":[3,5,67,7,8,8]
+        "resumen":[3,5,67,7,8,8],
+        "products":products
     }
     return render(request, "base.html",context=context)
